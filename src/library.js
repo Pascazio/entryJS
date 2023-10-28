22fa13036a49a7087e2805bae641f739c348435c
Donato Pascazio <pascazio@DESKTOP-2TAT4EQ.localdomain>
2023-10-28 13:02:49 +0200

    DOM quickfix.

--- src/library.js
+++ src/library.js
@@ -3,8 +3,17 @@ function sum(first, second) {
     return first + second;
 };
 
+//Function that allows you to see the result without writing console.log
+function showResult(first, second) {
+    const x = sum(first, second);
+    console.log(x);
+}
+
+
+
 //Export module sum
 module.exports = {
-    sum
+    sum, 
+    showResult,
 };
 
