2c3b3296e5534b58fbc68e6e7adc582f4f89881c
Antonio Servedio <Antonio.Servedio@emea.nttdata.com>
2023-11-02 18:34:27 +0100

    Create simple operation

--- src/library.js
+++ src/library.js
@@ -3,17 +3,39 @@ function sum(first, second) {
     return first + second;
 };
 
+function sott(first, second) {
+    return first - second;
+};
+
+function molt(first, second) {
+    return first * second;
+};
+
+function div(first, second) {
+    return first / second;
+};
+
 //Function that allows you to see the result without writing console.log
 function showResult(first, second) {
     const x = sum(first, second);
     console.log(x);
+    const y = sott(first, second);
+    console.log(y);
+    const z = molt(first, second);
+    console.log(z);
+    const k = div(first, second);
+    console.log(k);
 }
-
 
 
 //Export module sum
 module.exports = {
-    sum, 
+    sum,
+    sott,
+    molt,
+    div,
     showResult,
+    
+
 };
 
